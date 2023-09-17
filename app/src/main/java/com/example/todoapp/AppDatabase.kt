c9881fb502e330c044043d74774b7a7702c8b1d2
sanchay <sanchaysaxena.22@gmail.com>
2023-09-17 15:26:19 +0530

    ToDoApp

--- app/src/main/java/com/example/todoapp/AppDatabase.kt
+++ app/src/main/java/com/example/todoapp/AppDatabase.kt
@@ -6,25 +6,30 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [ToDoModel::class], version = 1)
-abstract class AppDatabase:RoomDatabase(){
-    abstract fun TodoDao():TodoDao
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun todoDao(): TodoDao
 
-    @Volatile
-    private var INSTANCE: AppDatabase? = null
 
-    fun getDatabase(context: Context): AppDatabase {
-        val tempInstance = INSTANCE
-        if (tempInstance != null) {
-            return tempInstance
-        }
-        synchronized(this) {
-            val instance = Room.databaseBuilder(
-                context.applicationContext,
-                AppDatabase::class.java,
-                "DB_NAME"
-            ).build()
-            INSTANCE = instance
-            return instance
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    DB_NAME
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
         }
     }
 }
--- app/src/main/java/com/example/todoapp/TaskActivity.kt
+++ app/src/main/java/com/example/todoapp/TaskActivity.kt
@@ -2,10 +2,20 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
+import com.example.todoapp.databinding.ActivityTaskBinding
 
+const val DB_NAME="todo.db"
 class TaskActivity : AppCompatActivity() {
+
+    val db by lazy {
+        AppDatabase.getDatabase(this)
+    }
+
+    private lateinit var binding:ActivityTaskBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_task)
+        binding=ActivityTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
